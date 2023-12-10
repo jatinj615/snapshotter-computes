c66116d830ce3fef9c1cbf76693451d99e035c93
Jatin Jain <jatinj615@gmail.com>
2023-12-10 08:49:40 +0530

    update: class names

--- eth_validator_states.py
+++ eth_validator_states.py
@@ -6,25 +6,21 @@ from typing import Union
 from redis import asyncio as aioredis
 
 from .utils.eth2 import BeaconNode
-from .utils.event_log_decoder import EventLogDecoder
-from .utils.models.message_models import TrackingWalletInteractionSnapshot
 from .utils.models.data_models import ValidatorStateSnapshot
 from snapshotter.utils.callback_helpers import GenericProcessorSnapshot
 from snapshotter.utils.default_logger import logger
 from .utils.helpers import getValidatorsSnapshots
-from snapshotter.utils.models.message_models import EthTransactionReceipt
 from snapshotter.utils.models.message_models import PowerloomSnapshotProcessMessage
-from snapshotter.utils.redis.redis_keys import epoch_txs_htable
 from snapshotter.utils.rpc import RpcHelper
 from snapshotter.settings.config import settings
 
 
-class TrackingWalletInteractionProcessor(GenericProcessorSnapshot):
+class ValidatorStateProcessor(GenericProcessorSnapshot):
     transformation_lambdas = None
 
     def __init__(self) -> None:
         self.transformation_lambdas = []
-        self._logger = logger.bind(module='TrackingWalletInteractionProcessor')
+        self._logger = logger.bind(module='ValidatorStateProcessor')
 
     async def compute(
         self,
